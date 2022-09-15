c5b27c258bb3c127932eb00757887b18e253be6d
KentoPuleo <104437768+KentoPuleo@users.noreply.github.com>
2022-09-15 10:26:25 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { TodolistComponent } from './todolist/todolist.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: '', component: TodolistComponent }]),
+  ],
+  declarations: [AppComponent, HelloComponent, TodolistComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
